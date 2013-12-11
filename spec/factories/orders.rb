718fe08e61d86677ad78efad733b98a5eb187b60
Colin Fcong <cfcong@gmail.com>
2013-12-11 19:20:26 +0000

    added tests for Orders controller

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -6,5 +6,14 @@ FactoryGirl.define do
     name { Faker::Name.first_name }
     price { (Faker::Number.number(3).to_f)/100 }
     number 1
+    factory :authenticated_order do
+      zip { Faker::Address.zip_code }
+      city { Faker::Address.city }
+      state { Faker::Address.state }
+      country { Faker::Address.country }
+      address_one { Faker::Address.street_address }
+      address_two { Faker::Address.secondary_address }
+      phone { Faker::PhoneNumber.cell_phone }
+    end
   end
 end
