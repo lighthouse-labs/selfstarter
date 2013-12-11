ca679294849e65c066d53059b4afc096da0acd6b
Colin Fcong <cfcong@gmail.com>
2013-12-11 02:54:19 +0000

    added additional forms required, fixed indentation, and fixed a bug where form would be submitted even before token was recieved

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,23 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
+    @order = Order.new(order_params)
+    @order.build_user(email: params[:email])
+    @order.price = Settings.price
+
+    if @order.save
+      redirect_to :root, notice: "Thanks champ"
+    else
+      render :new
+    end
   end
+
+  protected
+
+  def order_params
+    attrs = params.require(:order).permit(:zip, :city, :state, :country, :address_one, :address_two, :phone, :name)
+    attrs[:stripe_token] = params.require(:stripeToken)
+    attrs
+  end
+
 end
