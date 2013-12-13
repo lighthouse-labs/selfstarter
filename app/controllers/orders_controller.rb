b2524c0b1562fa71681ee893679b758dc2e15c6c
Colin Fcong <cfcong@gmail.com>
2013-12-13 22:03:43 +0000

    removed index action because there was no need for it in the orders_controller, limited the routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,6 @@
 class OrdersController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => :ipn
 
-  def index
-  end
-
   def new
     @user = User.new
     @order = Order.new
